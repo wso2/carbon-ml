20a487554030a3700745e5d66a23a3c7a790c1ba
SupunS <supuns@wso2.com>
2014-09-16 10:48:39 +0530

    Added the importData method

--- ml/ml-components/org.wso2.carbon.ml.dataset/src/main/java/org/wso2/carbon/ml/dataset/DatasetService.java
+++ ml/ml-components/org.wso2.carbon.ml.dataset/src/main/java/org/wso2/carbon/ml/dataset/DatasetService.java
@@ -1,22 +1,69 @@
 package org.wso2.carbon.ml.dataset;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
-public class DatasetService {
-	
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.ml.db.H2Connector;
 
-	public void importData(String source, String destination){
-		//TODO
+public class DatasetService {
+	private static final Log LOGGER = LogFactory.getLog(DatasetService.class);
+
+	public int importData(String source) throws Exception{
+		H2Connector h2Connector;
+		Connection connection;
+        try {
+	        h2Connector = H2Connector.initialize();
+	        connection = h2Connector.getConnection();
+	        //get the default uploading location
+	        ResultSet result=connection.createStatement().executeQuery("SELECT DATASET_UPLOADING_DIR FROM ML_CONFIGURATION");
+	        
+	        //if found
+	        if(result.first()){
+	        	 //update the dataset table
+		        String uri =result.getNString("DATASET_UPLOADING_DIR")+"/"+source;
+		        Statement insert = connection.createStatement();
+		        //insert the details to the table
+		        insert.execute("INSERT INTO ML_Dataset(URI) VALUES('"+uri+"');");
+		        //get the newest auto-generated-Id
+		        ResultSet latestID=insert.getGeneratedKeys();
+		        latestID.first();
+		        return Integer.parseInt(latestID.getNString(1));
+	        }
+	        else{
+	        	LOGGER.error("Deafult uploading location is not set in the ML_CONFIGURATION database table.");
+	        }
+        } catch (Exception e) {
+        	String msg="Error occured while updating the data-source details in the database."+e.getMessage();
+			LOGGER.error(msg,e);
+			throw new DatasetServiceException(msg);
+        }
+		return -1;
 	}
+	
 	
 	/*
 	 * Calculate summary stats and populate the database
 	 */
-	public void generateSummaryStats(String dataSource,int dataSourceID,int noOfRecords, int noOfIntervals, String seperator) throws Exception {
+	public void generateSummaryStats(String dataSource,int dataSourceID,int noOfRecords, int noOfIntervals, String seperator) throws DatasetServiceException {
 		DatasetSummary summary=new DatasetSummary();
-		summary.calculateSummary(dataSourceID, noOfRecords, noOfIntervals, seperator);
-		summary.updateDatabase(dataSourceID);
+		try {
+	        summary.calculateSummary(dataSourceID, noOfRecords, noOfIntervals, seperator);
+	        summary.updateDatabase(dataSourceID);
+        } catch (DatasetServiceException e) {
+        	String msg="Summary Statistics calculation failed."+e.getMessage();
+			LOGGER.error(msg,e);
+			throw new DatasetServiceException(msg);
+        } catch (Exception e) {
+        	String msg="Database update failed. "+e.getMessage();
+			LOGGER.error(msg,e);
+			throw new DatasetServiceException(msg);
+        }
+		
 	}
 	
 	/*
