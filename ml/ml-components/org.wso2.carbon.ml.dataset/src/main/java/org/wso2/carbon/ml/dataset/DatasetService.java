63845ecf1de61badb537330a3e7875d4b71e868e
Upul Bandara <upulbandara@gmail.com>
2014-09-18 18:01:08 +0530

    remove an unused method

--- ml/ml-components/org.wso2.carbon.ml.dataset/src/main/java/org/wso2/carbon/ml/dataset/DatasetService.java
+++ ml/ml-components/org.wso2.carbon.ml.dataset/src/main/java/org/wso2/carbon/ml/dataset/DatasetService.java
@@ -56,21 +56,6 @@ public class DatasetService {
 			throw new DatasetServiceException(msg);
         }
 	}
-	
-	public Feature[] getFeatures(int start, int numOfFeatures) {
-
-		// TODO:
-		Feature f1 = new Feature("age", false, new FeatureType(),
-				new ImputeOption());
-		Feature f2 = new Feature("salary", false, new FeatureType(),
-				new ImputeOption());
-
-		Feature[] features = new Feature[2];
-		features[0] = f1;
-		features[1] = f2;
-
-		return features;
-	}
 
 	public int importData(String source) throws Exception {
 		try {
