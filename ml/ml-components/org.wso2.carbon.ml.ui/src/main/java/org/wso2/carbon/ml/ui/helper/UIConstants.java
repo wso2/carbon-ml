35a3d917ff2df29e03035440d0cbb9ff07040c37
Upul Bandara <upulbandara@gmail.com>
2014-09-21 13:05:44 +0530

    d3.js library is added

---
+++ ml/ml-components/org.wso2.carbon.ml.ui/src/main/java/org/wso2/carbon/ml/ui/helper/UIConstants.java
@@ -0,0 +1,16 @@
+package org.wso2.carbon.ml.ui.helper;
+
+public class UIConstants {
+	
+	// data types related constants
+	public static final String CATEGORICAL = "CATEGORICAL";
+	public static final String NUMERICAL = "NUMERICAL";
+	
+	// impute options related constants
+	public static final String DISCARD = "DISCARD";
+	public static final String REPLACE_WTH_MEAN = "REPLACE_WTH_MEAN";
+	public static final String REGRESSION_IMPUTATION = "REGRESSION_IMPUTATION";
+	
+	//TODO: add others
+
+}
