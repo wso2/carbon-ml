5f8629ede7c657de972e85e436cc19fba6effeb0
lochana <lochanac@gmail.com>
2014-10-24 09:20:14 +0530

    10_24_2014_9_20AM

---
+++ ml/ml-components/org.wso2.carbon.ml.dataset/src/test/java/org/wso2/carbon/ml/dataset/DatasetServiceTest.java
@@ -0,0 +1,73 @@
+package org.wso2.carbon.ml.dataset;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DatasetServiceTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDatasetConfig() throws Exception {
+
+    }
+
+    @Test
+    public void testRegisterDataset() throws Exception {
+
+    }
+
+    @Test
+    public void testGenerateSummaryStats() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateFeature() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateDataType() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateImputeOption() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateIsIncludedFeature() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFeatures() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFeatureNames() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSamplePoints() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSummaryStats() throws Exception {
+
+    }
+}
