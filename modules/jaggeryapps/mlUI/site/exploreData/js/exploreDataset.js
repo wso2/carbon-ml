f6861da16de3ad71e0638e27d89e27e0c4d81bdd
SupunS <supuns@wso2.com>
2014-12-03 16:11:34 +0530

    NVD3 histogram added

--- modules/jaggeryapps/mlUI/site/exploreData/js/exploreDataset.js
+++ modules/jaggeryapps/mlUI/site/exploreData/js/exploreDataset.js
@@ -54,23 +54,17 @@ function drawPlotsAjax(){
         },
         success : function(data){
             var jsonObj  = JSON.parse(data);
-            var summary = "Mean: "+jsonObj.mean+ "&emsp;&emsp;&emsp; Median: "+jsonObj.median+ "&emsp;&emsp;&emsp; Std: "+jsonObj.std+ "&emsp;&emsp;&emsp; Skewness: "+jsonObj.skewness;
+            var summary = "Mean: "+jsonObj[0].mean+ "&emsp;&emsp;&emsp; Median: "+jsonObj[0].median+ "&emsp;&emsp;&emsp; Std: "+jsonObj[0].std+ "&emsp;&emsp;&emsp; Skewness: "+jsonObj[0].skewness;
             $("#histogram1Title").empty();
             $("#histogram1Title").append(numFeature1);
             $("#numFeature1Summary").empty();
             $("#numFeature1Summary").append(summary);
-            var frequencies = jsonObj.frequencies;       
+            var frequencies = jsonObj.frequencies;
             // transform dataset
-            var data = $.map(frequencies, function(value, index) {
-                return [value.frequency];
-            });
-            var bucketNames = $.map(frequencies, function(value, index) {
-                return [value.range];
-            });
-            drawHistogram(data,bucketNames,"#histogram1");
+            drawHistogram(jsonObj,"#histogram1");
         }
     });
-
+    
     $.ajax({
         type: "POST",
         url: "./ajax/fetchSummaryStatistics.jag",
@@ -79,22 +73,17 @@ function drawPlotsAjax(){
         },
         success : function(data){
             var jsonObj  = JSON.parse(data);
-            var summary = "Mean: " + jsonObj.mean + "&emsp;&emsp;&emsp; Median: " + jsonObj.median + "&emsp;&emsp;&emsp; Std: " + jsonObj.std + "&emsp;&emsp;&emsp; Skewness: " + jsonObj.skewness;
+            var summary = "Mean: "+jsonObj[0].mean+ "&emsp;&emsp;&emsp; Median: "+jsonObj[0].median+ "&emsp;&emsp;&emsp; Std: "+jsonObj[0].std+ "&emsp;&emsp;&emsp; Skewness: "+jsonObj[0].skewness;
             $("#histogram2Title").empty();
             $("#histogram2Title").append(numFeature2);
             $("#numFeature2Summary").empty();
             $("#numFeature2Summary").append(summary);
             var frequencies = jsonObj.frequencies;       
             // transform dataset
-            var data = $.map(frequencies, function(value, index) {
-                return [value.frequency];
-            });
-            var bucketNames = $.map(frequencies, function(value, index) {
-                return [value.range];
-            });
-            drawHistogram(data,bucketNames,"#histogram2");
+            drawHistogram(jsonObj,"#histogram2");
         }
     });
+
 }
 
 // drawing a simple scatter graph
@@ -111,14 +100,37 @@ function drawScatterPlot(data,cssClass,xLabel,yLabel){
     d3.select(cssClass).call(sg);
 };
 
-function drawHistogram(data,bucketNames,cssClass){
-    var hg = histogramUsingCalculatedFrequencies()
-        .data(data)
-        .bucketNames(bucketNames)
-        .width(600)
-        .height(125)
-        .yLabel('Frequencies')
-        .displayAxises(true);
-    $(cssClass).empty(); 
-    d3.select(cssClass).call(hg);
+function drawHistogram(data,divID){
+    $(divID+' svg').empty();
+    nv.addGraph(function() {
+    var chart = nv.models.linePlusBarChart()
+      .margin({top: 30, right: 60, bottom: 50, left: 70})
+      .x(function(d,i) { return i })
+      .y(function(d) { return d[1] })
+      .color(["#C99614"])
+      ;
+
+    chart.xAxis
+      .showMaxMin(false)
+      .tickFormat(function(d) {
+        return data[0].values[d][0];
+      });
+
+    chart.y1Axis
+      .tickFormat(d3.format(',f'));
+
+    chart.y2Axis
+      .tickFormat(function(d) { return '$' + d3.format(',f')(d) });
+
+    chart.bars.forceY([0]);
+
+    d3.select(divID+' svg')
+      .datum(data)
+      .transition().duration(500)
+      .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
 }
--- modules/jaggeryapps/mlUI/site/importData/js/importDataset.js
+++ modules/jaggeryapps/mlUI/site/importData/js/importDataset.js
@@ -160,7 +160,7 @@ function drawDataViewTable() {
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h);
-                
+                /*
                 svg.selectAll("rect")
                     .data(dataArray)
                     .enter()
@@ -176,6 +176,39 @@ function drawDataViewTable() {
                         return yScale(d);
                     })
                     .attr("fill", '#D59C0C');
+                    */
+                    $(divID+' svg').empty();
+                    nv.addGraph(function() {
+                        var chart = nv.models.linePlusBarChart()
+                          .margin({top: 30, right: 60, bottom: 50, left: 70})
+                          .x(function(d,i) { return i })
+                          .y(function(d) { return d[1] })
+                          .color(["#C99614"])
+                          ;
+
+                        chart.xAxis
+                          .showMaxMin(false)
+                          .tickFormat(function(d) {
+                            return jsonObj[0].values[d][0];
+                          });
+
+                        chart.y1Axis
+                          .tickFormat(d3.format(',f'));
+
+                        chart.y2Axis
+                          .tickFormat(function(d) { return '$' + d3.format(',f')(d) });
+
+                        chart.bars.forceY([0]);
+
+                        d3.select(this)
+                          .datum(jsonObj)
+                          .transition().duration(500)
+                          .call(chart);
+
+                        nv.utils.windowResize(chart.update);
+
+                        return chart;
+                    });
             }
         });
         
