671263a696d93af64e7e910b3316fc92602a40d5
SupunS <supuns@wso2.com>
2014-12-16 22:32:56 +0530

    bug fixed

---
+++ components/ml/org.wso2.carbon.ml.dataset/src/main/java/org/wso2/carbon/ml/dataset/internal/FilePathValidator.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.ml.dataset.internal;
+
+import java.util.regex.Pattern;
+
+/**
+ * Class contains methods for validating file paths.
+ */
+public class FilePathValidator {
+
+	private static final String FOLDER_PATH_REGEX = "([a-z]:\\/|\\/)([\\w\\/-]*)";
+	private static Pattern filePathPattern = Pattern.compile(FOLDER_PATH_REGEX);
+
+	/*
+	 * private Constructor to prevent any other class from instantiating.
+	 */
+	private FilePathValidator() {
+	}
+
+	/**
+	 * Checks whether the given path has the valid pattern.
+	 *
+	 * @param filePath Path to be checked for correct pattern
+	 * @return boolean indicating whether the given path has valid pattern
+	 */
+	protected static boolean isValid(String filePath) {
+		return filePathPattern.matcher(filePath).matches();
+	}
+}
---
+++ components/ml/org.wso2.carbon.ml.dataset/src/main/java/org/wso2/carbon/ml/dataset/internal/constants/FileFormats.java
@@ -0,0 +1,12 @@
+package org.wso2.carbon.ml.dataset.internal.constants;
+
+public class FileFormats {
+	public static final String CSV = "csv";
+	public static final String TEXT = "txt";
+	
+	/*
+     * private Constructor to prevent any other class from instantiating.
+     */
+    private FileFormats() {
+    }
+}
