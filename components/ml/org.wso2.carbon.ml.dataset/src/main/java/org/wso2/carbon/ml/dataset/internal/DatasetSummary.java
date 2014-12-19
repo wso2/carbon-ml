c0fffc7ccf43018d3650074634b7d279d2e8999c
SupunS <supuns@wso2.com>
2014-12-19 18:02:14 +0530

    Licence header modified to new format

--- components/ml/org.wso2.carbon.ml.dataset/src/main/java/org/wso2/carbon/ml/dataset/internal/DatasetSummary.java
+++ components/ml/org.wso2.carbon.ml.dataset/src/main/java/org/wso2/carbon/ml/dataset/internal/DatasetSummary.java
@@ -6,12 +6,12 @@
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -26,13 +26,12 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.commons.math3.random.EmpiricalDistribution;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
-import org.wso2.carbon.ml.dataset.dto.SamplePoints;
 import org.wso2.carbon.ml.dataset.exceptions.DatabaseHandlerException;
 import org.wso2.carbon.ml.dataset.exceptions.DatasetSummaryException;
 import org.wso2.carbon.ml.dataset.internal.constants.DatasetConfigurations;
 import org.wso2.carbon.ml.dataset.internal.constants.FeatureType;
-
-import java.io.*;
+import org.wso2.carbon.ml.dataset.dto.SamplePoints;
+import java.io.*;       
 import java.util.*;
 
 /**
