f849343ce3a498f9850282f2170234ead0854ae6
Nirmal Fernando <nirmal070125@gmail.com>
2015-02-27 16:43:05 +0530

    Introducing a thread pool to ML core.

--- components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/utils/MLConstants.java
+++ components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/utils/MLConstants.java
@@ -3,4 +3,5 @@ package org.wso2.carbon.ml.core.utils;
 public class MLConstants {
 
     public static final String TARGET_HOME_PROP = "target.home";
+    public static final String ML_THREAD_POOL_SIZE = "ml.thread.pool.size";
 }
