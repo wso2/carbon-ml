88185413446f7cc121f97575812b2f2e80ed922c
Nirmal Fernando <nirmal070125@gmail.com>
2015-03-02 11:38:36 +0530

    Refactoring and formatting

--- components/ml/org.wso2.carbon.ml.core/src/test/java/org/wso2/carbon/ml/core/impl/MLConfigurationParserTest.java
+++ components/ml/org.wso2.carbon.ml.core/src/test/java/org/wso2/carbon/ml/core/impl/MLConfigurationParserTest.java
@@ -35,26 +35,25 @@ public class MLConfigurationParserTest {
         Assert.assertEquals(settings.getSampleSize(), 10000);
         Assert.assertEquals(settings.getCategoricalThreshold(), 20);
         Assert.assertEquals(settings.getHistogramBins(), 20);
-        
+
         DataUploadSettings dataUploadSettings = config.getDataUploadSettings();
         Assert.assertEquals(dataUploadSettings.getInMemoryThreshold(), 1024);
         Assert.assertEquals(dataUploadSettings.getUploadLimit(), 20971520);
         Assert.assertEquals(dataUploadSettings.getUploadLocation(), "USER_HOME");
-        
+
         Assert.assertEquals(config.getDatabaseName(), "jdbc/WSO2ML_DB");
-        
+
         Properties properties = config.getProperties();
         Assert.assertEquals(properties.size(), 2);
         Assert.assertEquals(properties.containsKey(MLConstants.ML_THREAD_POOL_SIZE), true);
         Assert.assertEquals(properties.get(MLConstants.TARGET_HOME_PROP), "/tmp");
-        
-        
+
     }
-    
+
     @Test
     public void testMLConfigParserOnError() throws MLConfigurationParserException {
         try {
-            
+
             @SuppressWarnings("unused")
             MLConfigurationParser config = new MLConfigurationParser("src/test/resources/ml-config-temp.xml");
         } catch (Exception e) {
--- components/ml/org.wso2.carbon.ml.core/src/test/java/org/wso2/carbon/ml/core/impl/SummaryStatsGeneratorTest.java
+++ components/ml/org.wso2.carbon.ml.core/src/test/java/org/wso2/carbon/ml/core/impl/SummaryStatsGeneratorTest.java
@@ -16,63 +16,61 @@ import org.wso2.carbon.ml.core.utils.MLUtils;
 import org.wso2.carbon.ml.dataset.exceptions.MLConfigurationParserException;
 
 /**
- * TODO: incomplete test case, we need to have a small data-set and validate each
- * function.
+ * TODO: incomplete test case, we need to have a small data-set and validate each function.
  */
 public class SummaryStatsGeneratorTest {
     private SummaryStatsGenerator summaryGen;
-  @BeforeClass
-  public void beforeClass() throws URISyntaxException, MLInputAdapterException, MLConfigurationParserException {
-      MLInputAdapter inputAdapter = new FileInputAdapter();
-      
-      /*
-       * Dataset 
-       * Columns: 55
-       * Rows: 1000
-       */
-      String uriString = "src/test/resources/fcSample.csv";
-      URI uri ;
-      SamplePoints samplePoints = null;
-      InputStream in = null;
-      uri = new URI(System.getProperty("user.dir") + File.separator + uriString);
-      in = inputAdapter.readDataset(uri);
-      
-      try {
-          
-          // extract limited set of points
-         int size = 10000;
-         samplePoints = MLUtils.getSamplePoints(in, "csv", size);
-         Assert.assertNotNull(samplePoints);
-        
-    } catch (MLMalformedDatasetException e) {
-        Assert.assertNull(e);
+
+    @BeforeClass
+    public void beforeClass() throws URISyntaxException, MLInputAdapterException, MLConfigurationParserException {
+        MLInputAdapter inputAdapter = new FileInputAdapter();
+
+        /*
+         * Dataset Columns: 55 Rows: 1000
+         */
+        String uriString = "src/test/resources/fcSample.csv";
+        URI uri;
+        SamplePoints samplePoints = null;
+        InputStream in = null;
+        uri = new URI(System.getProperty("user.dir") + File.separator + uriString);
+        in = inputAdapter.readDataset(uri);
+
+        try {
+
+            // extract limited set of points
+            int size = 10000;
+            samplePoints = MLUtils.getSamplePoints(in, "csv", size);
+            Assert.assertNotNull(samplePoints);
+
+        } catch (MLMalformedDatasetException e) {
+            Assert.assertNull(e);
+        }
+        MLConfigurationParser config = new MLConfigurationParser("src/test/resources/ml-config.xml");
+        summaryGen = new SummaryStatsGenerator(config.getSummaryStatisticsSettings(), samplePoints);
     }
-      MLConfigurationParser config = new MLConfigurationParser("src/test/resources/ml-config.xml");
-      summaryGen = new SummaryStatsGenerator(config, samplePoints);
-  }
 
-  @Test
-  public void calculateDescriptiveStats() {
-    Assert.assertNotNull(summaryGen.calculateDescriptiveStats());
-  }
+    @Test
+    public void calculateDescriptiveStats() {
+        Assert.assertNotNull(summaryGen.calculateDescriptiveStats());
+    }
 
-  @Test
-  public void calculateNumericColumnFrequencies() {
-      Assert.assertNotNull(summaryGen.calculateNumericColumnFrequencies());
-  }
+    @Test
+    public void calculateNumericColumnFrequencies() {
+        Assert.assertNotNull(summaryGen.calculateNumericColumnFrequencies());
+    }
 
-  @Test
-  public void calculateStringColumnFrequencies() {
-      Assert.assertNotNull(summaryGen.calculateStringColumnFrequencies());
-  }
+    @Test
+    public void calculateStringColumnFrequencies() {
+        Assert.assertNotNull(summaryGen.calculateStringColumnFrequencies());
+    }
 
-  @Test
-  public void calculateIntervalFreqs() {
-      Assert.assertNotNull(summaryGen.calculateIntervalFreqs(0, 2));
-  }
+    @Test
+    public void calculateIntervalFreqs() {
+        Assert.assertNotNull(summaryGen.calculateIntervalFreqs(0, 2));
+    }
 
-  @Test
-  public void identifyColumnDataType() {
-      Assert.assertNotNull(summaryGen.identifyColumnDataType());
-  }
+    @Test
+    public void identifyColumnDataType() {
+        Assert.assertNotNull(summaryGen.identifyColumnDataType());
+    }
 }
