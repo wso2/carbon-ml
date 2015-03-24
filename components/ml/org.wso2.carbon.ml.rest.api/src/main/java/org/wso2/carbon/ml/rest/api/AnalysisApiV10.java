3226d92cf102a98052334d2c53d5ec76999af949
Nirmal Fernando <nirmal070125@gmail.com>
2015-03-24 10:16:17 +0530

    Adding comments.

--- components/ml/org.wso2.carbon.ml.rest.api/src/main/java/org/wso2/carbon/ml/rest/api/AnalysisApiV10.java
+++ components/ml/org.wso2.carbon.ml.rest.api/src/main/java/org/wso2/carbon/ml/rest/api/AnalysisApiV10.java
@@ -39,7 +39,7 @@ import org.wso2.carbon.ml.core.exceptions.MLAnalysisHandlerException;
 import org.wso2.carbon.ml.core.impl.MLAnalysisHandler;
 
 /**
- * This class is to handle REST verbs GET , POST and DELETE.
+ * WSO2 ML Analyses API. All the operations related to Analyses is implemented here.
  */
 @Path("/analyses")
 public class AnalysisApiV10 extends MLRestAPI {
@@ -51,6 +51,10 @@ public class AnalysisApiV10 extends MLRestAPI {
         mlAnalysisHandler = new MLAnalysisHandler();
     }
     
+    /**
+     * HTTP Options method implementation for analysis API.
+     * @return
+     */
     @OPTIONS
     public Response options() {
         return Response.ok()
