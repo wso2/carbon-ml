586539327469550e50d2a099edf2026c201ba80a
Nirmal Fernando <nirmal070125@gmail.com>
2015-03-26 17:53:17 +0530

    Adding beans

---
+++ components/ml/org.wso2.carbon.ml.rest.api/src/main/java/org/wso2/carbon/ml/rest/api/model/MLDatasetBean.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.ml.rest.api.model;
+
+import java.util.List;
+
+/**
+ * Represent a Data-set in ML.
+ */
+public class MLDatasetBean {
+
+    private long id;
+    private String name;
+    private String comments;
+    private String createdTime;
+    private List<MLVersionBean> versions;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getComments() {
+        return comments;
+    }
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+    public String getCreatedTime() {
+        return createdTime;
+    }
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+    public List<MLVersionBean> getVersions() {
+        return versions;
+    }
+    public void setVersions(List<MLVersionBean> versions) {
+        this.versions = versions;
+    }
+    
+}
