28503d7db970eca571a1f34da90b4033de9c4c23
Madawa Soysa <madawa.rc@gmail.com>
2015-07-16 16:38:47 +0530

    recommendation model builder and machine-learner xml

---
+++ components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/spark/recommendation/RecommendationModelBuilder.java
@@ -0,0 +1,134 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.wso2.carbon.ml.core.spark.recommendation;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel;
+import org.apache.spark.mllib.recommendation.Rating;
+import org.wso2.carbon.ml.commons.constants.MLConstants;
+import org.wso2.carbon.ml.commons.domain.MLModel;
+import org.wso2.carbon.ml.commons.domain.ModelSummary;
+import org.wso2.carbon.ml.commons.domain.Workflow;
+import org.wso2.carbon.ml.core.exceptions.AlgorithmNameException;
+import org.wso2.carbon.ml.core.exceptions.DatasetPreProcessingException;
+import org.wso2.carbon.ml.core.exceptions.MLModelBuilderException;
+import org.wso2.carbon.ml.core.interfaces.MLModelBuilder;
+import org.wso2.carbon.ml.core.internal.MLModelConfigurationContext;
+import org.wso2.carbon.ml.core.spark.algorithms.SparkModelUtils;
+import org.wso2.carbon.ml.core.spark.transformations.DoubleArrayToVector;
+import org.wso2.carbon.ml.core.utils.MLCoreServiceValueHolder;
+import org.wso2.carbon.ml.database.DatabaseService;
+
+import java.util.Map;
+
+public class RecommendationModelBuilder extends MLModelBuilder{
+	private static final Log log = LogFactory.getLog(RecommendationModelBuilder.class);
+
+	public RecommendationModelBuilder(MLModelConfigurationContext context) {
+		super(context);
+	}
+
+	public MLModel build(MLModelConfigurationContext context) throws MLModelBuilderException {
+		JavaSparkContext sparkContext = null;
+		DatabaseService databaseService = MLCoreServiceValueHolder.getInstance().getDatabaseService();
+		MLModel mlModel = new MLModel();
+
+		sparkContext = context.getSparkContext();
+
+		//pre-processing dataset
+		JavaRDD<Rating> trainingData = null;
+		try {
+			trainingData = RecommendationUtils.preProcess(context);
+		} catch (DatasetPreProcessingException e) {
+			e.printStackTrace();
+		}
+
+		CollaborativeFiltering collaborativeFiltering = new CollaborativeFiltering();
+		MatrixFactorizationModel model = collaborativeFiltering.trainExplicit(trainingData, 10, 20,RecommendationConstants.DEFAULT_LAMBDA, 10);
+		Rating[] recommendedProducts = collaborativeFiltering.recommendProducts(model,1,RecommendationConstants.DEFAULT_NUMBER_OF_ITEMS);
+		for (Rating recommendedProduct : recommendedProducts) {
+			System.out.println(recommendedProduct.user() + " " + recommendedProduct.product());
+		}
+		return null;
+	}
+
+	@Override
+	public MLModel build() throws MLModelBuilderException {
+		MLModelConfigurationContext context = getContext();
+		DatabaseService databaseService = MLCoreServiceValueHolder.getInstance().getDatabaseService();
+		try {
+			Workflow workflow = context.getFacts();
+			long modelId = context.getModelId();
+
+			// apply pre processing
+			JavaRDD<Rating> trainingData = RecommendationUtils.preProcess(context);
+
+			MLModel mlModel = new MLModel();
+			mlModel.setAlgorithmName(workflow.getAlgorithmName());
+			mlModel.setAlgorithmClass(workflow.getAlgorithmClass());
+			mlModel.setFeatures(workflow.getFeatures());
+			mlModel.setResponseVariable(workflow.getResponseVariable());
+			mlModel.setEncodings(context.getEncodings());
+			mlModel.setNewToOldIndicesList(context.getNewToOldIndicesList());
+			mlModel.setResponseIndex(-1);
+
+			MatrixFactorizationModel model;
+
+			// build a recommendation model according to user selected algorithm
+			MLConstants.RECOMMENDATION_ALGORITHM
+					recommendation_algorithm = MLConstants.RECOMMENDATION_ALGORITHM.valueOf(workflow.getAlgorithmName());
+			switch (recommendation_algorithm) {
+				case COLLABORATIVE_FILTERING:
+					model = buildCollaborativeFilteringModel(trainingData, workflow, mlModel);
+					break;
+				default:
+					throw new AlgorithmNameException("Incorrect algorithm name: " + workflow.getAlgorithmName()
+					                                 + " for model id: " + modelId);
+			}
+			return mlModel;
+		} catch (Exception e) {
+			throw new MLModelBuilderException("An error occurred while building unsupervised machine learning model: "
+			                                  + e.getMessage(), e);
+		}
+	}
+
+	private MatrixFactorizationModel buildCollaborativeFilteringModel(JavaRDD<Rating> trainingData, Workflow workflow,
+	                                                                  MLModel mlModel) throws MLModelBuilderException {
+
+		try {
+			Map<String, String> parameters = workflow.getHyperParameters();
+			CollaborativeFiltering collaborativeFiltering = new CollaborativeFiltering();
+			MatrixFactorizationModel model = collaborativeFiltering
+					.trainExplicit(trainingData, Integer.parseInt(parameters.get(MLConstants.RANK)),
+					               Integer.parseInt(parameters.get(MLConstants.NUM_ITERATIONS)),
+					               Double.parseDouble(MLConstants.LAMBDA),
+					               Integer.parseInt(parameters.get(MLConstants.NUM_BLOCKS)));
+
+			//TODO: mlModel.setModel(Externalizable model)
+
+			return model;
+		} catch(Exception e) {
+			throw new MLModelBuilderException("An error occurred while building k-means model: " + e.getMessage(), e);
+		}
+	}
+}
