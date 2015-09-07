c400bf47670d37a03f9b77832f19562d6141a6a4
Nirmal Fernando <nirmal070125@gmail.com>
2015-09-07 09:26:45 +0530

    Removing a mistakenly added comment.

--- components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/factories/ModelBuilderFactory.java
+++ components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/factories/ModelBuilderFactory.java
@@ -30,7 +30,6 @@ public class ModelBuilderFactory {
 
     public static MLModelBuilder buildModelBuilder(String algorithmType, MLModelConfigurationContext context)
             throws MLInputValidationException {
-        // common transformations
         AlgorithmType type = AlgorithmType.getAlgorithmType(algorithmType);
 
         MLModelBuilder modelBuilder = null;
