1537061fc0b205231e11d63de78527fab21df534
Nirmal Fernando <nirmal070125@gmail.com>
2015-09-07 12:31:03 +0530

    Performing https://wso2.org/jira/browse/ML-232

---
+++ components/extensions/org.wso2.carbon.ml.custom.adapter/README.md
@@ -0,0 +1,9 @@
+ML Custom Adapter Extension
+===========================
+
+This OSGi bundle demonstrates how you could extend WSO2 ML input/output adapters.
+
+1. This bundle is a fragment bundle of org.wso2.carbon.ml.core bundle.
+2. You can build the bundle and copy it into {ML_HOME}/repository/components/dropis folder.
+3. Once you copy the bundle, you could configure new adapter using {ML_HOME}/repository/conf/machine-learner.xml file.
+
---
+++ components/extensions/org.wso2.carbon.ml.custom.adapter/src/main/java/org/wso2/carbon/ml/custom/adapter/input/CustomMLInputAdapter.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.ml.custom.adapter.input;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.wso2.carbon.ml.core.exceptions.MLInputAdapterException;
+import org.wso2.carbon.ml.core.interfaces.MLInputAdapter;
+
+/**
+ * This is a sample custom extension for a ML input adapter. You could follow the same pattern and implement a ML output
+ * adapter too.
+ */
+public class CustomMLInputAdapter implements MLInputAdapter {
+
+    @Override
+    public InputStream read(String path) throws MLInputAdapterException {
+        try {
+            FileInputStream inputStream = new FileInputStream(new File(path));
+            return inputStream;
+        } catch (FileNotFoundException e) {
+            throw new MLInputAdapterException(String.format("Failed to read the data-set from uri %s: %s", path, e), e);
+        } catch (IllegalArgumentException e) {
+            throw new MLInputAdapterException(String.format("Failed to read the data-set from uri %s: %s", path, e), e);
+        }
+    }
+}
