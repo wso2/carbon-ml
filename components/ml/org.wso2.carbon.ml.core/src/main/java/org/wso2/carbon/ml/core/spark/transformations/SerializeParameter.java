6cef1e1a364118a21b1a9a6d6226d0585bbce09a
Misgana <misgananegassi@gmail.com>
2016-06-16 15:35:37 +0200

    class for (de) seriallizing Object of hyper parameters and

--- components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/spark/transformations/SerializeParameter.java
+++ components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/spark/transformations/SerializeParameter.java
@@ -1,41 +1,30 @@
 package org.wso2.carbon.ml.core.spark.transformations;
 
-import org.wso2.carbon.ml.commons.constants.MLConstants;
-import org.wso2.carbon.ml.core.utils.Util;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
 
 /**
  * Created by pekasa on 16.06.16.
  */
-public class SerializeParameter {
-    public static void main(String []args) throws IOException {
+public class SerializeParameter implements java.io.Serializable{
 
-        Util serialize = new Util();
-        List<Map<String, String>> paramsBaseAlgorithms= getParameters();
-
-        String PARAMETERS_BASE_ALGORITHM = serialize.serializeObjectToString(paramsBaseAlgorithms);
-        // how to get parameters of meta algorithm
+    /** Write object to Base64 string. */
+    public static String toString( Object o ) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream( baos );
+        oos.writeObject( o );
+        oos.close();
+        return java.util.Base64.getEncoder().encodeToString(baos.toByteArray());
     }
 
-    public static List<Map<String, String>> getParameters (){
-        Map<String, String>  parameters = new HashMap<String, String>();
-        parameters.put(MLConstants.IMPURITY , "entropy");
-        parameters.put(MLConstants.MAX_DEPTH,"5" );
-        parameters.put(MLConstants.MAX_BINS, "32" );
-        parameters.put(MLConstants.NUM_CLASSES, "3");
-        parameters.put(MLConstants.NUM_TREES, "3");
-        parameters.put(MLConstants.FEATURE_SUBSET_STRATEGY, "auto");
-        parameters.put(MLConstants.SEED, "12345");
-        List<Map<String, String>> parametersList = new ArrayList<Map<String, String>>();
-        parametersList.add(parameters);
-        parametersList.add(parameters);
-
-
-        return parametersList ;
+    /** Read the object from Base64 string. */
+    public static Object fromString( String s ) throws IOException ,
+            ClassNotFoundException {
+        byte [] data = java.util.Base64.getDecoder().decode( s );
+        ObjectInputStream ois = new ObjectInputStream(
+                new ByteArrayInputStream(  data ) );
+        Object o  = ois.readObject();
+        ois.close();
+        return o;
     }
+
 }
