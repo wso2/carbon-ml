d71c886326cd29a6b23ba27861eb19a867a4f35a
Misgana <misgananegassi@gmail.com>
2016-07-12 17:31:56 +0200

    basemodelbuilder updated

--- components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/spark/algorithms/BaseModelsBuilder.java
+++ components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/spark/algorithms/BaseModelsBuilder.java
@@ -10,11 +10,12 @@ import org.wso2.carbon.ml.commons.domain.MLModel;
 import org.wso2.carbon.ml.commons.domain.Workflow;
 import org.wso2.carbon.ml.core.exceptions.AlgorithmNameException;
 import org.wso2.carbon.ml.core.exceptions.MLModelBuilderException;
+import org.wso2.carbon.ml.core.interfaces.MLModelBuilder;
+import org.wso2.carbon.ml.core.internal.MLModelConfigurationContext;
 import org.wso2.carbon.ml.core.spark.models.MLClassificationModel;
 import org.wso2.carbon.ml.core.spark.models.MLDecisionTreeModel;
 import org.wso2.carbon.ml.core.spark.models.MLRandomForestModel;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,7 +23,16 @@ import java.util.Map;
 /**
  * Created by pekasa on 07.06.16.
  */
-public class BaseModelsBuilder {
+public class BaseModelsBuilder extends MLModelBuilder {
+    public BaseModelsBuilder(MLModelConfigurationContext context) {
+        super(context);
+    }
+
+
+    @Override
+    public MLModel build() throws MLModelBuilderException {
+        return null;
+    }
 
 
     /**
@@ -34,48 +44,33 @@ public class BaseModelsBuilder {
       */
 
 
-        public MLModel buildBaseModels(String algorithmName, JavaRDD<LabeledPoint> trainingData,
-               Map<String, String> algorithmParameters) throws MLModelBuilderException {
+        public MLModel buildBaseModels(Workflow workflow, MLModel mlModel, String algorithmName, JavaRDD<LabeledPoint> trainingData,
+                                       Map<String, String> algorithmParameters) throws MLModelBuilderException {
 
+            try {
+                MLModelConfigurationContext context = getContext();
+                mlModel.setEncodings(context.getEncodings());
+                mlModel.setAlgorithmName(algorithmName);
 
-        MLModel mlModel = new MLModel();
-
-        try {
-           // Workflow workflow = context.getFacts();
-            Workflow workflow = new Workflow();
-            workflow.setHyperParameters(algorithmParameters);
-            List<Map<String, Integer>> encodings = new ArrayList<>();
-            Map<String, Integer> mappings = new HashMap<String, Integer>();
-            encodings.add(mappings);
-
-            // remove from cache
-            // create a deployable MLModel object
-            mlModel.setAlgorithmName(algorithmName);
-            mlModel.setAlgorithmClass(MLConstants.CLASSIFICATION);
-           // mlModel.setFeatures(workflow.getIncludedFeatures());
-            //mlModel.setResponseVariable(workflow.getResponseVariable());
-            mlModel.setEncodings(encodings);
-            //mlModel.setNewToOldIndicesList(context.getNewToOldIndicesList());
-
-            Map<Integer, Integer> categoricalFeatureInfo;
 
 
             // build a machine learning model according to user selected algorithm
+            Map<Integer, Integer> categoricalFeatureInfo;
             MLConstants.SUPERVISED_ALGORITHM supervisedAlgorithm = MLConstants.SUPERVISED_ALGORITHM.valueOf(algorithmName);
              switch (supervisedAlgorithm) {
 
                 case DECISION_TREE:
-                    categoricalFeatureInfo = getCategoricalFeatureInfo(encodings);
+                    categoricalFeatureInfo = getCategoricalFeatureInfo(context.getEncodings());
                     mlModel = buildDecisionTreeModel(trainingData, workflow,
-                            mlModel, categoricalFeatureInfo);
+                            mlModel, categoricalFeatureInfo, algorithmParameters);
                     break;
                 case RANDOM_FOREST_CLASSIFICATION:
-                    categoricalFeatureInfo = getCategoricalFeatureInfo(encodings);
+                    categoricalFeatureInfo = getCategoricalFeatureInfo(context.getEncodings());
                     mlModel = buildRandomForestClassificationModel(trainingData, workflow,
-                            mlModel, categoricalFeatureInfo);
+                            mlModel, categoricalFeatureInfo, algorithmParameters);
                     break;
                  case NAIVE_BAYES:
-                     mlModel = buildNaiveBayesModel(trainingData,workflow ,mlModel);
+                     mlModel = buildNaiveBayesModel(trainingData,workflow ,mlModel, algorithmParameters);
                      break;
 
 
@@ -91,16 +86,6 @@ public class BaseModelsBuilder {
     }
 
 
-    private Map<Integer, Integer> getCategoricalFeatureInfo(List<Map<String, Integer>> encodings) {
-        Map<Integer, Integer> info = new HashMap<Integer, Integer>();
-        // skip the response variable which is at last
-        for (int i = 0; i < encodings.size() - 1; i++) {
-            if (encodings.get(i).size() > 0) {
-                info.put(i, encodings.get(i).size());
-            }
-        }
-        return info;
-    }
 
 
     /**
@@ -114,16 +99,16 @@ public class BaseModelsBuilder {
      */
     private MLModel buildDecisionTreeModel(
             JavaRDD<LabeledPoint> trainingData, Workflow workflow, MLModel mlModel,
-             Map<Integer, Integer> categoricalFeatureInfo)
+             Map<Integer, Integer> categoricalFeatureInfo,Map<String, String> algorithmParameters)
             throws MLModelBuilderException {
         try {
             Map<String, String> hyperParameters = workflow.getHyperParameters();
             DecisionTree decisionTree = new DecisionTree();
             DecisionTreeModel decisionTreeModel = decisionTree.train(trainingData,
-                    Integer.parseInt(hyperParameters.get(MLConstants.NUM_CLASSES)),
-                    categoricalFeatureInfo, hyperParameters.get(MLConstants.IMPURITY),
-                    Integer.parseInt(hyperParameters.get(MLConstants.MAX_DEPTH)),
-                    Integer.parseInt(hyperParameters.get(MLConstants.MAX_BINS)));
+                    getNoOfClasses(mlModel),
+                    categoricalFeatureInfo, algorithmParameters.get(MLConstants.IMPURITY),
+                    Integer.parseInt(algorithmParameters.get(MLConstants.MAX_DEPTH)),
+                    Integer.parseInt(algorithmParameters.get(MLConstants.MAX_BINS)));
 
             // remove from cache
             trainingData.unpersist();
@@ -140,18 +125,18 @@ public class BaseModelsBuilder {
 
     }
     private MLModel buildRandomForestClassificationModel(JavaRDD<LabeledPoint> trainingData, Workflow workflow, MLModel mlModel,
-             Map<Integer, Integer> categoricalFeatureInfo) throws MLModelBuilderException {
+             Map<Integer, Integer> categoricalFeatureInfo, Map<String, String> algorithmParameters) throws MLModelBuilderException {
         try {
-            Map<String, String> hyperParameters = workflow.getHyperParameters();
+            //Map<String, String> hyperParameters = workflow.getHyperParameters();
             RandomForestClassifier randomForestClassifier = new RandomForestClassifier();
             final RandomForestModel randomForestModel = randomForestClassifier.train(trainingData,
-                    Integer.parseInt(hyperParameters.get(MLConstants.NUM_CLASSES)),
-                    categoricalFeatureInfo, Integer.parseInt(hyperParameters.get(MLConstants.NUM_TREES)),
-                    hyperParameters.get(MLConstants.FEATURE_SUBSET_STRATEGY),
-                    hyperParameters.get(MLConstants.IMPURITY),
-                    Integer.parseInt(hyperParameters.get(MLConstants.MAX_DEPTH)),
-                    Integer.parseInt(hyperParameters.get(MLConstants.MAX_BINS)),
-                    Integer.parseInt(hyperParameters.get(MLConstants.SEED)));
+                    getNoOfClasses(mlModel),
+                    categoricalFeatureInfo, Integer.parseInt(algorithmParameters.get(MLConstants.NUM_TREES)),
+                    algorithmParameters.get(MLConstants.FEATURE_SUBSET_STRATEGY),
+                    algorithmParameters.get(MLConstants.IMPURITY),
+                    Integer.parseInt(algorithmParameters.get(MLConstants.MAX_DEPTH)),
+                    Integer.parseInt(algorithmParameters.get(MLConstants.MAX_BINS)),
+                    Integer.parseInt(algorithmParameters.get(MLConstants.SEED)));
 
             // remove from cache
             trainingData.unpersist();
@@ -178,12 +163,11 @@ public class BaseModelsBuilder {
      * @param mlModel Deployable machine learning model
      * @throws MLModelBuilderException
      */
-    private MLModel buildNaiveBayesModel(JavaRDD<LabeledPoint> trainingData, Workflow workflow, MLModel mlModel) throws MLModelBuilderException {
+    private MLModel buildNaiveBayesModel(JavaRDD<LabeledPoint> trainingData, Workflow workflow, MLModel mlModel, Map<String, String> algorithmParameters) throws MLModelBuilderException {
         try {
-            Map<String, String> hyperParameters = workflow.getHyperParameters();
             NaiveBayesClassifier naiveBayesClassifier = new NaiveBayesClassifier();
             NaiveBayesModel naiveBayesModel = naiveBayesClassifier.train(trainingData,
-                    Double.parseDouble(hyperParameters.get(MLConstants.LAMBDA)));
+                    Double.parseDouble(algorithmParameters.get(MLConstants.LAMBDA)));
 
             // remove from cache
             trainingData.unpersist();
@@ -196,6 +180,27 @@ public class BaseModelsBuilder {
             throw new MLModelBuilderException("An error occurred while building naive bayes model: " + e.getMessage(),
                     e);
         }
+    }
+
+    private Map<Integer, Integer> getCategoricalFeatureInfo(List<Map<String, Integer>> encodings) {
+        Map<Integer, Integer> info = new HashMap<Integer, Integer>();
+        // skip the response variable which is at last
+        for (int i = 0; i < encodings.size() - 1; i++) {
+            if (encodings.get(i).size() > 0) {
+                info.put(i, encodings.get(i).size());
+            }
+        }
+        return info;
+    }
+
+    private int getNoOfClasses(MLModel mlModel) {
+        if (mlModel.getEncodings() == null) {
+            return -1;
+        }
+        int responseIndex = mlModel.getEncodings().size() - 1;
+        return mlModel.getEncodings().get(responseIndex) != null ? mlModel.getEncodings().get(responseIndex).size()
+                : -1;
+
     }
 
 
