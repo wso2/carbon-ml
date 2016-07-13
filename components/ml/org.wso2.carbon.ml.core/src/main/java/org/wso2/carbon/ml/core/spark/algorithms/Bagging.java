e860d97044cc1cac53c9a4111fac05c3c6b53d0c
Misgana <misgananegassi@gmail.com>
2016-07-13 20:13:04 +0200

    bug  in train method resolved

--- components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/spark/algorithms/Bagging.java
+++ components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/spark/algorithms/Bagging.java
@@ -5,7 +5,10 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.classification.ClassificationModel;
+import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.regression.LabeledPoint;
+import org.apache.spark.rdd.RDD;
 import org.wso2.carbon.ml.commons.domain.MLModel;
 import org.wso2.carbon.ml.commons.domain.Workflow;
 import org.wso2.carbon.ml.core.exceptions.MLModelBuilderException;
@@ -15,6 +18,7 @@ import org.wso2.carbon.ml.core.internal.MLModelConfigurationContext;
 import org.wso2.carbon.ml.core.utils.Util;
 import scala.Tuple2;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -23,24 +27,27 @@ import java.util.Map;
 /**
  * Created by pekasa on 29.06.16.
  */
-public class Bagging {
+public class Bagging implements Serializable, ClassificationModel {
+
     private List<MLModel> levelZeroModels = new ArrayList<MLModel>();
 
-   public void train(MLModelConfigurationContext modelConfContext, JavaSparkContext sparkContext, Workflow workflow, MLModel mlModel, long modelId, JavaRDD<LabeledPoint> trainData, List<String> baseModels,
+   public void train(MLModelConfigurationContext context, JavaSparkContext sparkContext, Workflow workflow, MLModel mlModel, long modelId, JavaRDD<LabeledPoint> trainData, List<String> baseModels,
           List<Map<String, String>> paramsBaseAlgorithms, Integer seed) throws NullPointerException, MLModelHandlerException,
             MLModelBuilderException {
 
-       BaseModelsBuilder build = new BaseModelsBuilder(modelConfContext);
+       BaseModelsBuilder build = new BaseModelsBuilder();
        int cnt = 0;
        for (String model : baseModels) {
-           int idx = 0;
+           MLModel baseModel = new MLModel();
            JavaRDD<LabeledPoint> bootstrapSample = trainData.sample(true, 1.0);
-           MLModel baseModel = build.buildBaseModels(workflow,mlModel, model, bootstrapSample, paramsBaseAlgorithms.get(cnt));
+
+           baseModel = build.buildBaseModels(context, workflow, new MLModel(), model, bootstrapSample, paramsBaseAlgorithms.get(cnt));
            cnt++;
            // get list of models trained on whole Dataset
            levelZeroModels.add(baseModel);
 
        }
+
 
 
     }
@@ -56,8 +63,9 @@ public class Bagging {
             datapointList.add(datapoint);
             List<Double> datapointPredictions = new ArrayList<Double>();
             for (MLModel model : levelZeroModels) {
-                Predictor predictor = new Predictor(modelId, model, datapointList);
-                List<?> predictions = predictor.predict();
+                List<?> predictions = new ArrayList<>();
+                Predictor predictor = new Predictor(modelId, model, datapointList, 0.0,true);
+                predictions = predictor.predict();
                 datapointPredictions.add(Double.valueOf(predictions.get(0).toString()));
             }
             Map<Double, Integer> cardinalityMap = CollectionUtils.getCardinalityMap(datapointPredictions);
@@ -69,6 +77,7 @@ public class Bagging {
                 }
             }
         }
+
         List<Tuple2<Double, Double>> list = new ArrayList<Tuple2<Double, Double>>();
         for (int j = 0; j < resultPredictions.size(); j++) {
             list.add(new Tuple2<Double, Double>(resultPredictions.get(j), labelsList.get(j)));
@@ -76,5 +85,20 @@ public class Bagging {
         }
         return sparkContext.parallelizePairs(list);
 
+    }
+
+    @Override
+    public RDD<Object> predict(RDD<Vector> rdd) {
+        return null;
+    }
+
+    @Override
+    public double predict(Vector vector) {
+        return 0;
+    }
+
+    @Override
+    public JavaRDD<Double> predict(JavaRDD<Vector> javaRDD) {
+        return null;
     }
 }
