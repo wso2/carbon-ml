db2658d5fed96e6ffde44e6c0684d20cc6e35f0f
Misgana <misgananegassi@gmail.com>
2016-08-09 22:00:59 +0200

    some new helper methods

--- components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/utils/Util.java
+++ components/ml/org.wso2.carbon.ml.core/src/main/java/org/wso2/carbon/ml/core/utils/Util.java
@@ -111,8 +111,6 @@ public class Util {
     public List<String[]> labeledpointToListStringArray(JavaRDD<LabeledPoint> rddata) {
         List<String[]> dataToBePredicted = new ArrayList<String[]>();
         List<LabeledPoint> list = rddata.collect();
-        //List<Map<Integer, String>> reversedEncodingsList = reverse(encodings);
-
 
         for(LabeledPoint item : list ){
             String[] labeledPointFeatures = new String[item.features().size()];
@@ -120,9 +118,22 @@ public class Util {
             for(int k= 0; k<vector.length; k++){
                 labeledPointFeatures[k] = (Double.toString(vector[k]));
        }
-           // labeledPointFeatures = decoder(vector, reversedEncodingsList);
+            dataToBePredicted.add(labeledPointFeatures);
 
+        }
 
+        return dataToBePredicted;
+
+    }
+    public List<String[]> labeledpointToStringArray(List<LabeledPoint> list) {
+        List<String[]> dataToBePredicted = new ArrayList<String[]>();
+
+        for(LabeledPoint item : list ){
+            String[] labeledPointFeatures = new String[item.features().size()];
+            double[] vector = item.features().toArray();
+            for(int k= 0; k<vector.length; k++){
+                labeledPointFeatures[k] = (Double.toString(vector[k]));
+            }
             dataToBePredicted.add(labeledPointFeatures);
 
         }
